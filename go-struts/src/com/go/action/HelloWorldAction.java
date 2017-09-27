270b973c66ae6a0c89385bfe1cbe2fc4e16d4524
sirzc <zhouchao151724@163.com>
2017-09-27 19:16:43 +0800

    this is struts2

---
+++ go-struts/src/com/go/action/HelloWorldAction.java
@@ -0,0 +1,20 @@
+package com.go.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport{
+ 
+	public String index() throws Exception {
+		System.out.println(getActionMessages()+"this is index method"); 
+		return SUCCESS;
+	}
+	
+	@Override
+	public String execute() throws Exception {
+		System.out.println(getActionMessages()+"this is execute method"); 
+		return SUCCESS;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
